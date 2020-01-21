c5b6731b4c47eaf11da25d0587564defbf50a950
Eryk Slowinski <e.slowinski92@gmail.com>
2020-01-21 18:53:56 +0100

    recent changes

--- main.js
+++ main.js
@@ -9,19 +9,15 @@ const publicRepos = document.querySelector('.public-repos');
 const userFollowers = document.querySelector('.followers');
 const accountCreated = document.querySelector('.account');
 const spinner = document.querySelector('.lds-spinner')
-let endpoint;
+const endpoint = 'https://api.github.com/users/';
 
-
-btnSubmit.addEventListener('click', (e) => {
-    e.preventDefault();
-    endpoint = `https://api.github.com/users/${inputField.value}`;
-    displayData();
-})
-
-async function displayData() {
-    spinner.style.opacity = '1';
-    const response = await fetch(endpoint);
+async function getData(user) {
+    const response = await fetch(`${endpoint}${user}`);
     const data = await response.json();
+    displayData(data);
+}
+
+function displayData(data) {
     image.src = data.avatar_url;
     image.style.border = '1px solid rgb(27, 26, 26, .7)';
     userInfo.textContent = `User name: ${data.name}`;
@@ -33,3 +29,31 @@ async function displayData() {
     accountCreated.textContent = `Account created at: ${data.created_at}`;
     spinner.style.opacity = '0';
 }
+
+function clearData() {
+    image.src = '';
+    image.style.border = 'none';
+    userInfo.textContent = '';
+    idInfo.textContent = '';
+    locationInfo.textContent = '';
+    bioInfo.textContent = '';
+    publicRepos.textContent = '';
+    userFollowers.textContent = '';
+    accountCreated.textContent = '';
+}
+
+function handleError(error) {
+    console.log(error);
+    clearData();
+    spinner.style.opacity = '0';
+    image.src = 'img/404-error-not-found.png';
+    userInfo.textContent = 'Check if you typed correct user name.';
+    userInfo.style.margin = 'auto';
+}
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    spinner.style.opacity = '1';
+    getData(inputField.value).catch(handleError);
+    clearData();
+})
