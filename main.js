9a18b174ae33751094bbeae0650ac8d575aae8fc
Eryk Slowinski <e.slowinski92@gmail.com>
2020-01-21 17:44:09 +0100

    Recent changes

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const bioInfo = document.querySelector('.bio');
 const publicRepos = document.querySelector('.public-repos');
 const userFollowers = document.querySelector('.followers');
 const accountCreated = document.querySelector('.account');
+const spinner = document.querySelector('.lds-spinner')
 let endpoint;
 
 
@@ -17,17 +18,18 @@ btnSubmit.addEventListener('click', (e) => {
     displayData();
 })
 
-
 async function displayData() {
-    // user.textContent = 'Loading...';
+    spinner.style.opacity = '1';
     const response = await fetch(endpoint);
     const data = await response.json();
     image.src = data.avatar_url;
+    image.style.border = '1px solid rgb(27, 26, 26, .7)';
     userInfo.textContent = `User name: ${data.name}`;
     idInfo.textContent = `User ID: ${data.id}`;
     locationInfo.textContent = `User location: ${data.location}`;
     bioInfo.textContent = `User bio: ${data.bio}`;
     publicRepos.textContent = `User public repositories: ${data.public_repos}`;
     userFollowers.textContent = `User followers: ${data.followers}`;
-    accountCreated.textContent = `Account created: ${data.created_at}`;
+    accountCreated.textContent = `Account created at: ${data.created_at}`;
+    spinner.style.opacity = '0';
 }
